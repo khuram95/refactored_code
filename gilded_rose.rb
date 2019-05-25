0cafa201966d2e6d2553cf695ba1c7711721078b
khuram95 <khurams1995@gmail.com>
2019-05-25 23:59:51 +0500

    code refactored

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,56 +1,53 @@
 class GildedRose
   def initialize(items)
     @items = items
+    @maximum_quality = 50
+    @minimum_quality = 0
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      case item.name
+      when 'Aged Brie'
+        item.quality += 1 if item.quality < @maximum_quality
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        update_backstage_passes_quality(item)
+      when 'Conjured Mana Cake'
+        item.quality-2 < @minimum_quality ? item.quality = @minimum_quality : item.quality-=2
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
+      if item.sell_in == 0
+        expired(item)
+        next
       end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_sell_in(item)
     end
   end
-end
+
+  def update_backstage_passes_quality(item)
+    case item.sell_in
+    when 6..10
+      item.quality += 2
+    when 1..5
+      item.quality += 3
+    when 0
+      item.quality = @minimum_quality
+    end
+  end
+
+  def update_sell_in(item)
+    item.sell_in-=1 if item.sell_in > 0 and item.name != "Sulfuras, Hand of Ragnaros"
+  end
+
+  def expired(item)
+    return if item.quality < @minimum_quality
+    case item.name
+    when 'Aged Brie'
+      item.quality-1 < @minimum_quality ? item.quality = @minimum_quality : item.quality-=1
+    when 'Conjured Mana Cake'
+      item.quality-2 < @minimum_quality ? item.quality = @minimum_quality : item.quality-=2
+    end
+  end
+ end
 
 class Item
   attr_accessor :name, :sell_in, :quality
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,26 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
-    end
-  end
+ describe '#update_quality' do
+   it 'does not change the name' do
+     items = [Item.new('foo', 0, 0)]
+     GildedRose.new(items).update_quality()
+     expect(items[0].name).to eq 'fixme'
+   end
+   it 'quality should not be increased/decreased' do
+     items = [Item.new('Sulfuras, Hand of Ragnaros',0,80)]
+     GildedRose.new(items).update_quality()
+     expect(items[0].quality).to eq 80
+   end
+   it 'quality should be increased' do
+     items = [Item.new('Backstage passes to a TAFKAL80ETC concert',3,80)]
+     GildedRose.new(items).update_quality()
+     expect(items[0].quality).to eq 83
+   end
+   it 'quality should be zero' do
+     items = [Item.new('Backstage passes to a TAFKAL80ETC concert',0,80)]
+     GildedRose.new(items).update_quality()
+     expect(items[0].quality).to eq 0
+   end
+ end
 end
